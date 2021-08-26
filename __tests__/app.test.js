f09b1ee13704ec62421dac4c0995a39daa7a765e
Kalan Prudhomme <cleaver9@gmail.com>
2021-08-26 13:14:12 -0700

    working on auth test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -4,7 +4,19 @@ import request from 'supertest';
 import app from '../lib/app.js';
 
 describe('demo routes', () => {
-  beforeEach(() => {
+  beforeAll(() => {
     return setup(pool);
+  });
+
+  it('signs up user', async() => {
+    const user = {
+      username: 'bob',
+      password: 'fourtwenty'
+    };
+    const { body } = await request(app)
+      .post('api/v1/auth/signup')
+      .send(user);
+
+    expect(body).toEqual({ id: '1', ...user });
   });
 });

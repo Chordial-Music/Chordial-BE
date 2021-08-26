81235317a9099788b2e0631c6e0366f03ffd602b
Casey Cameron <mcasey.cameron@gmail.com>
2021-08-26 13:49:27 -0700

    Refactor auth

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,20 +3,23 @@ import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
 
+const agent = request.agent(app);
+
 describe('demo routes', () => {
   beforeAll(() => {
     return setup(pool);
   });
 
-  it('signs up user', async() => {
+  it('signs up user', async () => {
     const user = {
       username: 'bob',
       password: 'fourtwenty'
     };
-    const { body } = await request(app)
+
+    const res = await request(app)
       .post('api/v1/auth/signup')
       .send(user);
 
-    expect(body).toEqual({ id: '1', ...user });
+    expect(res.body).toEqual({ id: '1', ...user });
   });
 });
